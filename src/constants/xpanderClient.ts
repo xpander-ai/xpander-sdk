267511f842e61fe42317feac05cd1e28a6058164
Moriel Pahima <moriel@xpander.ai>
2024-11-12 10:12:39 +0200

    feat(RDT-1227): base url

--- src/constants/xpanderClient.ts
+++ src/constants/xpanderClient.ts
@@ -3,9 +3,9 @@
  *
  * This is the main endpoint for sending inbound API calls to xpanderAI.
  */
-// export const DEFAULT_BASE_URL = 'https://inbound.xpander.ai';
+export const DEFAULT_BASE_URL = 'https://inbound.xpander.ai';
 // Alternative local URL for testing:
-export const DEFAULT_BASE_URL = 'http://127.0.0.1:9004';
+// export const DEFAULT_BASE_URL = 'http://127.0.0.1:9004';
 
 /**
  * Identifier for custom agent configurations in xpanderAI.
